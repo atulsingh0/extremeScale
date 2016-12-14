af557499b77d1ef1e8baf200f1e0f128d19246d8
Atul Singh <victimizeit@gmail.com>
2016-12-14 18:55:51 +0530

    Create CustomerBean.java

---
+++ Work/CustomerBean.java
@@ -0,0 +1,10 @@
+import java.io.Serializable;
+
+
+public class CustomerBean implements Serializable{
+    String id;
+    String firstName;
+    String surname;
+    String address;
+    String phoneNumber;
+}
