26e58010a36d2b99ca3e7c52c8beb58e79de3d78
Atul Singh <victimizeit@gmail.com>
2016-12-14 18:56:19 +0530

    Create OrderBean.java

---
+++ Work/OrderBean.java
@@ -0,0 +1,11 @@
+import java.io.Serializable;
+public class OrderBean implements Serializable {
+    String orderNumber;
+    java.util.Date date;
+    String customerName;
+    String customerId;
+    String itemName;
+    int quantity;
+    double price;
+	   
+}
